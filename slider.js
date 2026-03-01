41d11196d0897a4c9ea687af6ae77e391b8ad04c
wildanardians580-spec <wildanardians580@gmail.com>
2026-03-01 23:11:07 +0700

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,20 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+}
+
+function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}
+
+function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+}
+
+// Auto slide tiap 4 detik
+setInterval(nextSlide, 4000);
